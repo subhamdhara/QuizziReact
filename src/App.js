d0f839756aa8b09031f8131c3235b63e9dc7e31b
Bhaskar Dhara <dharabhaskar@gmail.com>
2022-05-16 18:35:12 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Login from './login/login.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <div className="title">
+            <h1>Quizzi</h1>
+        </div>
+        <div class="top-nav">
+            <a href="register/register.html" className="btn">
+                Register
+            </a>
+            <a  href="login" className="btn">
+                Login
+            </a >
+            
+        </div>
+    </header>
+    <main>
+        <div className="left">
+            <h1>
+                Hello!
+            </h1>
+            <h2>
+                It's quiz time
+            </h2>
+        </div>
+        <div classN ame="right">
+            <a href="register/register.js" class="btn">
+                Register
+            </a>
+            <a href="login/login.js" class="btn">
+                Login
+            </a>
+        </div>
+    </main>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/login/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App.css'
+import './login.css'
+
+function Login() {
+  return (
+    <div className="Login">
+      
+    </div>
+  );
+}
+
+export default Login;
